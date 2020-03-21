08e07828f305deb5d989f99406b3058b5da73555
jakob <jakob.n.neufeld@gmail.com>
2020-03-21 19:44:44 +0100

    Addition works now up to 64 bit

--- Sources/BitwiseMath/main.swift
+++ Sources/BitwiseMath/main.swift
@@ -21,22 +21,34 @@ struct ManMath: ParsableCommand {
     }
     @Option(name: [.customLong("operation", withSingleDash: false), .customShort("o")], help: "The operand") var operation: MathOption
     @Flag(name: [.customShort("v"), .customLong("verbose", withSingleDash: false)], help: "Activate verbose kode. ASll the operations are printed.") var verbose: Bool
-    func run() throws {
-        let argl = arg1
-        let argr = arg2
-        var carry = 0
-        var newcarry = 0
+    func addBit(left: Int, right: Int, carry: Int) -> (Int, Int) {
         var result = 0
+        var newcarry = 0
+        let bitleft = left & 0b1
+        let bitright = right & 0b1
+        result = (bitleft ^ bitright) ^ carry
+        newcarry = bitleft & bitright
+        return (result, newcarry)
+    }
+    func run() throws {
+        var argl = arg1
+        var argr = arg2
+        var carry = 0
+        var result = 0
+        var oppResult = 0
         guard operation == .addtion else {
             throw "Subtraction is not supported."
         }
-        let bitleft = argl & 0b1
-        let bitright = argr & 0b1
         
-        result = bitleft ^ bitright
-        newcarry = bitleft & bitright
-        result = result ^ carry
-        carry = newcarry
+        for i in 0...63 {
+            (oppResult, carry) = addBit(left: argl, right: argr, carry: carry)
+            result = result | (oppResult << i)
+            argl = argl >> 1
+            argr = argr >> 1
+            verboseLog("result bit \(i) \(binary: oppResult), carry: \(binary: carry)")
+        }
+        
+       
         print("Result: \(binary: result), carry: \(binary: carry)")
         
         
